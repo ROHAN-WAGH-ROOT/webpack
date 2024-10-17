340449625ac8de4fac2a5499edfcd96f6e215b6f
rohan <rohanwagh234@gmail.com>
2024-10-17 11:53:26 +0530

    added loader for image ,svg, png and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from "react";
+import "./style.css";
+import img from "./a.png";
+import svg from './a.svg';
 
 export default function App() {
   return (
-    <div>react webpack app</div>
-  )
+    <>
+      <h1>react webpack app</h1>
+      <img src={img} width={100} height={100} alt="example" />
+      <img src={svg} alt="example" width={100} height={100} />
+    </>
+  );
 }
